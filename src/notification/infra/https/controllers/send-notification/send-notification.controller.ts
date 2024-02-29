aa454ccdd1fcbd151265001f9518d939ffe34d0f
herlanderbento <herlanderbento19@gmail.com>
2024-02-29 17:10:11 +0000

    new feature

--- src/notification/infra/https/controllers/send-notification/send-notification.controller.ts
+++ src/notification/infra/https/controllers/send-notification/send-notification.controller.ts
@@ -1,8 +1,7 @@
-import { type Request, type Response } from "express";
-import { StatusCodes } from "http-status-codes";
-import { SendNotificationUseCase } from "~/notification/application";
-import { sendNotificationValidated } from "./validator";
-import { EntityID } from "~/_shared/domain";
+import { type Request, type Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { SendNotificationUseCase } from '~/notification/application';
+import { sendNotificationValidated } from './validator';
 
 export class SendNotificationController {
   constructor(
@@ -11,12 +10,8 @@ export class SendNotificationController {
 
   async handle(request: Request, response: Response): Promise<Response> {
     const input = await sendNotificationValidated(request);
-    
-    const output = await this.sendNotificationUseCase.execute({
-      recipientId: new EntityID(input.recipientId),
-      title: input.title,
-      content: input.content,
-    });
+
+    const output = await this.sendNotificationUseCase.execute(input);
 
     return response.status(StatusCodes.CREATED).json({
       data: output,
--- src/notification/infra/https/controllers/send-notification/validator/send-notification.validator.ts
+++ src/notification/infra/https/controllers/send-notification/validator/send-notification.validator.ts
@@ -1,5 +1,6 @@
-import { type Request } from "express";
-import * as Yup from "yup";
+import { type Request } from 'express';
+import * as Yup from 'yup';
+import { SendNotificationInput } from '~/notification/application';
 
 export async function sendNotificationValidated(request: Request) {
   const schema = Yup.object().shape({
@@ -8,8 +9,8 @@ export async function sendNotificationValidated(request: Request) {
     content: Yup.string().required(),
   });
 
-  return await schema.validate(request.body, {
+  return (await schema.validate(request.body, {
     abortEarly: false,
     stripUnknown: true,
-  });
+  })) as unknown as SendNotificationInput;
 }
